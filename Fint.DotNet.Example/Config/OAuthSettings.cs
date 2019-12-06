065c19cb9f4f26df1032d9ae5363b029738c6ca9
Asgeir Storesund Nilsen <asgeir@fintlabs.no>
2019-12-06 11:31:54 +0100

    Update OAuthSettings.cs

--- Fint.DotNet.Example/Config/OAuthSettings.cs
+++ Fint.DotNet.Example/Config/OAuthSettings.cs
@@ -7,7 +7,7 @@ namespace Fint.DotNet.Example
         public const string clientId = "8e0fb9b9-2d88-47f0-8f0c-5e49dbc2ecd1";
         public const string clientSecret = "i8ccIfOrRm9ykpz1iDX6cNCsg5mfV8rtEluc9C6TncFpdsUJqae0vcirVlecR9pZ8ihGyCcdxRLk85s7huFakw";
         public const string username = "eksempel@client.fintlabs.no";
-        public const string password = "zGfisLdDiiL7coPsdagZjecAPwox4tQg";
+        public const string password = "pJS5jifpYz4tTu68UDvDqUePgvsiCApO";
         public const string scope = "fint-client";
         public const string accessTokenUri = "https://idp.felleskomponent.no/nidp/oauth/nam/token";
         public const string felleskomponentUri = "https://beta.felleskomponent.no";
